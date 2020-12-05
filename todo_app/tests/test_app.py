61183c8e078df0785b97e5445d06ca8f67c4758d
gm <bobinaki@gmail.com>
2020-12-05 10:44:11 +0000

    Step 3 - Autogenerate Items and minor optimisations

--- todo_app/tests/test_app.py
+++ todo_app/tests/test_app.py
@@ -5,14 +5,14 @@ from datetime import datetime
 
 _TEST_BOARD_NAME = 'test_board_todotest'
 
-def create_test_item(id, title, due_date, description, status):
-    return Item({Item.item_id: id, Item.item_title: title, Item.item_due_date: due_date, Item.item_description: description, Item.item_status: status, Item.item_last_updated: datetime.utcnow().strftime(r'%Y-%m-%dT%H:%M:%SZ') })
+def create_test_item(id, title, due_date, description, status, last_updated_date = datetime.utcnow().strftime(r'%Y-%m-%dT%H:%M:%SZ')):
+    return Item({Item.item_id: id, Item.item_title: title, Item.item_due_date: due_date, Item.item_description: description, Item.item_status: status, Item.item_last_updated: last_updated_date })
 
 def init_temp_board():
     
-    _status_todo_id = '123a'
-    _status_doing_id = '345b'
-    _status_done_id = '678c'
+    _status_todo_id = '1'
+    _status_doing_id = '2'
+    _status_done_id = '3'
 
     test_board_statuses = {
         _status_todo_id: BoardStatus({BoardStatus.boardstatus_id: _status_todo_id, BoardStatus.boardstatus_name: 'To Do'}),
@@ -20,30 +20,20 @@ def init_temp_board():
         _status_done_id: BoardStatus({BoardStatus.boardstatus_id: _status_done_id, BoardStatus.boardstatus_name: 'Done'})
     }
     
-    test_board = Board({Board.board_id: '123', Board.board_name: _TEST_BOARD_NAME, Board.board_statuses: test_board_statuses})
+    test_board = Board({Board.board_id: '1', Board.board_name: _TEST_BOARD_NAME, Board.board_statuses: test_board_statuses})
 
-    test_items = [
-        # To Do
-        create_test_item('0', 'Test 0', '02/02/2021', 'Test 0 is all right', _status_todo_id), 
-        create_test_item('0', 'Test 1', '02/02/2021', 'Test 1 is good', _status_todo_id),
-        create_test_item('0', 'Test 2', '03/02/2021', 'Test 2 is cool', _status_todo_id),
-        # Doing
-        create_test_item('0', 'Test 3', '04/02/2021', 'Test 3 is great',_status_doing_id),
-        create_test_item('0', 'Test 4', '05/02/2021', 'Test 4 is fantastic',_status_doing_id),
-        create_test_item('0', 'Test 5', '06/02/2021', 'Test 5 is extraordinary',_status_doing_id),
-        create_test_item('0', 'Test 6', '07/02/2021', 'Test 6 is gigantic',_status_doing_id),
-        create_test_item('0', 'Test 7', '08/02/2021', 'Test 7 is exotic',_status_doing_id),
-        create_test_item('0', 'Test 8', '09/02/2021', 'Test 8 is another league',_status_doing_id),
-        create_test_item('0', 'Test 9', '10/02/2021', 'Test 9 is beyond this world',_status_doing_id),
-        create_test_item('0', 'Test 10', '11/02/2021', 'Test 10 is infinitely amazing',_status_doing_id),
-        #Done
-        create_test_item('0', 'Test 11', '01/12/2020', 'Test 11 is peculiar',_status_done_id),
-        create_test_item('0', 'Test 12', '02/12/2020', 'Test 12 is weird',_status_done_id),
-        create_test_item('0', 'Test 13', '02/12/2020', 'Test 13 is bananas',_status_done_id),
-        create_test_item('0', 'Test 14', '02/12/2020', 'Test 14 is crazy',_status_done_id),
-        create_test_item('0', 'Test 17', '02/12/2020', 'Test 15 is mental',_status_done_id),
-        create_test_item('0', 'Test 16', '02/12/2020', 'Test 16 is insane',_status_done_id),
-    ]
+    test_items = []
+    for x in range(0,16):
+        status = ''
+        if x < 3:
+            status = _status_todo_id
+        elif x < 10:
+            status = _status_doing_id
+        else:
+            status = _status_done_id
+        
+        test_items.append(create_test_item(str(x), 'Test %d' % x, '02/02/2021', ' My Test %d is nice' % x, status))
+
     return test_board, test_items
 
 @pytest.fixture()
@@ -75,7 +65,7 @@ def test_get_todo_items(my_viewmodel):
 
 
 def test_get_doing_items(my_viewmodel):
-    assert len(my_viewmodel.doing_items) == 8
+    assert len(my_viewmodel.doing_items) == 7
 
 def test_get_done_items(my_viewmodel):
     assert len(my_viewmodel.done_items) == 6
